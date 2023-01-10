e92fa774a8f6e126d5b161cc4a2385e10a5d2ea2
louisamurray <121989665+louisamurray@users.noreply.github.com>
2023-01-11 12:41:12 +1300

    1

---
+++ js/cards.js
@@ -0,0 +1,273 @@
+buildSetsScreen(spanishCards);
+
+var selectedCards = [];
+
+function buildSetsScreen(cards){
+	
+	document.getElementById("pageTitle").innerHTML = "Select Card Decks";
+	var deckNameHeader = document.getElementById("deckName");
+	deckNameHeader.innerHTML = cards.name;
+	var setsScreen = document.getElementById("sets");
+	
+	var orderButton = new Togglable(document.getElementById("setsFooter"), "Order: Sorted", "Order: Random");
+	orderButton.getElement().className += " orderButton footerButton right";
+	
+	var startButton = document.getElementById("startButton");
+	
+	for(var i in cards.sets){
+		var lengthStr = "";
+		if(cards.sets[i].cards.length == 1){
+			lengthStr = "<span>1 card</span>"
+		} else {
+			lengthStr = "<span>"+cards.sets[i].cards.length+" cards</span>"
+		}
+		cards.sets[i].toggleButton = new Togglable(setsScreen, cards.sets[i].name + lengthStr, "", updateSetSelections);
+	}
+	
+	deckNameHeader.onclick = function(){
+		var selectedSetCount = 0;
+		for(var i in cards.sets){
+			if(cards.sets[i].toggleButton.isToggled()){
+				selectedSetCount++;
+			}
+		}
+		
+		for(var i in cards.sets){
+			if(selectedSetCount > cards.sets.length / 2){
+				cards.sets[i].toggleButton.set(false);
+			} else {
+				cards.sets[i].toggleButton.set(true);
+			}
+		}
+	};
+	
+	function updateSetSelections(){
+		var selectedSetCount = 0;
+		for(var i in cards.sets){
+			if(cards.sets[i].toggleButton.isToggled()){
+				selectedSetCount++;
+			}
+		}
+		if(selectedSetCount > 0){
+			startButton.disabled = false;
+		} else {
+			startButton.disabled = true;
+		}
+	}
+	
+	startButton.onclick = function(){
+		if(startButton.disabled){
+			return false;
+		}
+
+		selectedCards = [];
+		
+		for(var i in cards.sets){
+			if(cards.sets[i].toggleButton.isToggled()){
+				for(var a in cards.sets[i].cards){
+					var card = cards.sets[i].cards[a];
+					if(!(card instanceof Array)){
+						selectedCards.push({
+							 set: cards.sets[i]
+							,isMulti: true
+							,deck: cards
+							,name: card.name
+							,cards: card.cards
+							,number: a
+						});
+					} else {
+						selectedCards.push({
+							 set: cards.sets[i]
+							,isMulti: false
+							,deck: cards
+							,front: card[0]
+							,back: card[1]
+							,number: a
+						});
+					}
+				}
+			}
+		}
+
+		var shuffleCards = orderButton.isToggled();
+		
+		startQuiz(shuffleCards);
+	}
+}
+
+function showSetsScreen(){
+	document.getElementById("setScreen").style.display = "block";
+	document.getElementById("quizScreen").style.display = "none";
+	document.getElementById("menuButton").style.display = "none";
+	document.getElementById("pageTitle").innerHTML = "Select Card Decks";
+}
+
+function startQuiz(shuffleCards){
+	document.getElementById("setScreen").style.display = "none";
+	document.getElementById("quizScreen").style.display = "block";
+	var menuButton = document.getElementById("menuButton");
+	menuButton.style.display = "inline-block";
+	menuButton.onclick = showSetsScreen;
+	
+	var cardStack = selectedCards;
+	if(shuffleCards){
+		cardStack = shuffle(cardStack);
+	}
+	var correctCards = [];
+	var wrongCards = [];
+	
+	var cardNameEl = document.getElementById("questionName");
+	var cardFrontEl = document.getElementById("cardFront");
+	var cardBackEl = document.getElementById("cardBack");
+	var hiddenCardEl = document.getElementById("hiddenCard");
+	hiddenCardEl.onclick = showSolution;
+	
+	var pageTitleEl = document.getElementById("pageTitle");
+	
+	var correctButton = document.getElementById("correctButton");
+	correctButton.onclick = wasCorrect;
+	var wrongButton = document.getElementById("wrongButton");
+	wrongButton.onclick = wasWrong;
+	
+	var progressBarEl = document.getElementById("quizProgressBar");
+	var progressBarContainer = document.getElementById("quizProgress");
+	
+	var pass = 0;
+	var passLength = cardStack.length;
+	
+	next();
+	
+	function next(){
+		hiddenCardEl.dataset.hidden = true;
+		correctButton.disabled = true;
+		wrongButton.disabled = true;
+		
+		if(cardStack.length == 0){
+			if(wrongCards.length == 0){
+				showSetsScreen();
+				return;
+			}
+			cardStack = wrongCards;
+			if(shuffleCards){
+				cardStack = shuffle(cardStack);
+			}
+			wrongCards = [];
+			pass++;
+			passLength = cardStack.length;
+		}
+		
+		document.getElementById("quiz").scrollTop = 0;
+		
+		progressBarEl.innerHTML = "Card "+(passLength - cardStack.length + 1)+" of "+passLength;
+		progressBarEl.style.width = ((passLength - cardStack.length + 1) / passLength) * progressBarContainer.offsetWidth + "px";
+		
+		var card = cardStack[0];
+		
+		pageTitleEl.innerHTML = card.set.name + " - #"+ (~~(card.number) + 1);
+
+		if(card.isMulti){ // Multi card
+
+			document.getElementById("singleCard").style.display = "none";
+			document.getElementById("multiCard").style.display = "block";
+
+			if(card.name){
+				document.getElementById("multiName").innerHTML = card.name;
+				document.getElementById("multiName").style.display = "block";
+			} else {
+				document.getElementById("multiName").style.display = "none";
+			}
+			
+			
+			var multiCardsContainer = document.getElementById("multiCards");
+			multiCardsContainer.innerHTML = "";
+
+			var hiddenCount = card.cards.length;
+			var multiCards = [];
+
+			for(var i in card.cards){
+				multiCards.push(new MultiCard(card.cards[i][0], card.cards[i][1], function(){
+					hiddenCount--;
+					if(hiddenCount <= 0){
+						correctButton.disabled = false;
+						wrongButton.disabled = false;
+					}
+				}));
+			}
+			
+			
+			
+		} else { // Single card
+
+			document.getElementById("singleCard").style.display = "block";
+			document.getElementById("multiCard").style.display = "none";
+		
+			cardFrontEl.innerHTML = card.front;
+			cardBackEl.innerHTML = card.back;
+		}
+	}
+
+	function MultiCard(frontText, backText, onShow){
+		var hidden = true;
+
+		var front = document.createElement("p");
+		front.className = "multiFront";
+		front.innerHTML = frontText;
+		
+		var back = document.createElement("div");
+		back.className = "multiBack";
+		back.dataset.hidden = true;
+		var backTitleEl = document.createElement("p");
+		backTitleEl.className = "multiBackTitle";
+		backTitleEl.innerHTML = "Show";
+		var backTextEl = document.createElement("p");
+		backTextEl.className = "multiBackText";
+		backTextEl.innerHTML = backText;
+
+		back.appendChild(backTitleEl);
+		back.appendChild(backTextEl);
+
+		back.onclick = function(){
+			if(hidden){
+				show();
+				hidden = false;
+				onShow();
+			}
+		}
+
+		var multiCardsContainer = document.getElementById("multiCards");
+		multiCardsContainer.appendChild(front);
+		multiCardsContainer.appendChild(back);
+
+		function show(){
+			
+			back.dataset.hidden = "false";
+
+			backTitleEl.style.display = "none";
+			backTextEl.style.display = "block";
+		
+		}
+		
+		return {
+			 isHidden: function(){return hidden;}
+			,show: show
+		};
+	}
+	
+	function showSolution(){
+		hiddenCardEl.dataset.hidden = false;
+		
+		correctButton.disabled = false;
+		wrongButton.disabled = false;
+		
+	}
+	
+	function wasCorrect(){
+		correctCards.push(cardStack.shift());
+		next();
+	}
+	
+	function wasWrong(){
+		wrongCards.push(cardStack.shift());
+		next();
+	}
+}
---
+++ js/spanish.js
@@ -0,0 +1,175 @@
+var spanishCards = {
+	name: "English - Spanish",
+	sets: [ {
+		name: "Level 1",
+		cards: [
+			[ "The cars are new.", "Los coches son nuevos." ],
+			[ "How are you?", "¿Cómo estás?" ],
+			[ "Catalonia Square is in the center of Barcelona.",
+				"La plaza de Cataluña está en el centro de Barcelona."
+],
+			[ "Switzerland is a beautiful country.", "Suiza es un país bonito." ],
+			[ "With me", "Conmigo" ],
+			[ "Elena lives in Barcelona.", "Elena vive en Barcelona." ],
+			[ "Next year", "El año que viene" ],
+			[ "Are you Mr. and Mrs. Ballesteros?",
+				"¿Son ustedes los señores Ballesteros?"
+			],
+			[ "Excuse me, where is the cafeteria?",
+				"Perdón, ¿dónde está la cafetería?"
+			],
+			[ "Helen is Spanish and she is also Swiss.",
+				"Elena es española, y también es suiza."
+			]
+		]
+	}, 
+  {
+	name: "Level 2",
+	cards: [
+		[ "I am sorry, but I don't speak Spanish very well.", "Lo siento, pero no hablo muy bien el español." ],
+		[ "The weather is hot today.", "Hoy hace calor." ],
+		[ "I have a headache.", "Tengo dolor de cabeza." ],
+		[ "The food is delicious.", "La comida está deliciosa." ],
+		[ "I'm going to the store.", "Voy a la tienda." ],
+		[ "The movie was very interesting.", "La película era muy interesante." ],
+		[ "I'm going to bed now.", "Ahora voy a acostarme." ],
+		[ "I'll see you tomorrow.", "Te veo mañana." ],
+		[ "I'm looking for my keys.", "Estoy buscando mis llaves." ]
+	]
+},
+{
+	name: "Level 3",
+	cards: [
+		[ "I would like to order a coffee, please.", "Me gustaría pedir un café, por favor." ],
+		[ "Can you pass the salt, please?", "¿Puedes pasar la sal, por favor?" ],
+		[ "I'm going to the beach this weekend.", "Voy a la playa este fin de semana." ],
+		[ "I'm allergic to peanuts.", "Soy alérgico a las cacahuetes." ],
+		[ "The concert was sold out.", "El concierto estaba agotado." ],
+		[ "I have a meeting at 2 o'clock.", "Tengo una reunión a las 2 de la tarde." ],
+		[ "I'm sorry, I can't come to the phone right now.", "Lo siento, no puedo atender el teléfono ahora mismo." ],
+		[ "I'm going to the gym later.", "Voy al gimnasio más tarde." ],
+		[ "I need to renew my passport.", "Necesito renovar mi pasaporte." ]
+	]
+},
+{
+	name: "Level 4",
+	cards: [
+		[ "Can you recommend a good restaurant in the area?", "¿Puedes recomendar un buen restaurante en la zona?" ],
+		[ "I'm going to visit my parents in Madrid next week.", "Voy a visitar a mis padres en Madriz la próxima semana." ],
+		[ "I'm sorry, I don't have enough money.", "Lo siento, no tengo suficiente dinero." ],
+		[ "I'm learning to play the guitar.", "Estoy aprendiendo a tocar la guitarra." ],
+		[ "I'm studying to become a doctor.", "Estoy estudiando para ser médico." ],
+		[ "I'm tired of always having to do the dishes.", "Estoy cansado de tener que lavar los platos siempre." ]
+]
+},
+{
+	name: "Level 5 - Madrileño Slang",
+	cards: [
+		[ "What's up, buddy?", "¿Qué tal, pibe?" ],
+		[ "I like it! Let's go out tonight!", "¡Me mola! ¡Vámonos de fiesta esta noche!" ],
+		[ "I'm starving, man!", "¡Tengo un hambre de lobo, tío!" ],
+		[ "I'm so tired.", "Estoy muerto." ],
+		[ "I'm broke.", "Estoy arruinado." ],
+		[ "What's the plan, dude?", "¿Cuál es el plan, tronco?" ],
+		[ "Let's grab some beers.", "Cojamos unas cañas." ],
+		[ "That party at your house was epic!", "¡Esa fiesta en tu keli fue la leche!" ]
+	]
+},
+{
+	name: "Level 6 - More Madrileño Slang",
+	cards: [
+		[ "That motorcycle is really cool.", "Que guay esa motocicleta." ],
+		[ "When you get to know him you gonna think he's great.", "Cuando lo conozcas te va a parecer super guay." ],
+		[ "Sure, I'll come by later and help you.", "Vale, más tarde voy y te ayudo." ],
+		[ "Alright! Then let's head out.", "¡Venga! Entonces vamos saliendo." ],
+		[ "Sure man, it's nothing. I'll even invite you a drink.", "Claro hombre, no pasa nada. Es más, te invito una copa." ],
+		[ "Should we spend the long weekend at the beach?", "¿Pasamos el puente en la playa?" ],
+[ "A really pretty girl came to say hello to you.", "Vino a saludarte una tia muy guapa." ],
+[ "You have a some foreign friends, right?", "Tú tienes unos amigos guiris, ¿Verdad?" ],
+[ "Keep an eye on the kids.", "Échale un ojo a los chavales." ]
+]
+},
+
+   {
+		name: "Verb Conjugations",
+		cards: [ {
+				cards: [
+					[ "enter", "entrar" ],
+					[ "I enter", "entro" ],
+					[ "You enter", "entras" ],
+					[ "He enters", "entra" ],
+					[ "We enter", "entramos" ],
+					[ "You enter (plural)", "entráis" ],
+					[ "They enter (plural)", "entran" ]
+				]
+			}, {
+				cards: [
+					[ "eat", "comer" ],
+					[ "I eat", "como" ],
+					[ "You eat", "comes" ],
+					[ "She eats", "come" ],
+					[ "We eat", "comemos" ],
+					[ "You eat (plural)", "coméis" ],
+					[ "They eat (plural)", "comen" ]
+				]
+			}, {
+				cards: [
+					[ "write", "escribir" ],
+					[ "I write", "escribo" ],
+					[ "You write", "escribes" ],
+					[ "He writes", "escribe" ],
+					[ "We write", "escribimos" ],
+					[ "You write (plural)", "escribís" ],
+					[ "They write (plural)", "escriben" ]
+				]
+			}
+			,
+		]
+	},
+   {
+		name: "Multis",
+		cards: [ {
+			name: "Los Números",
+			cards: [
+				[ "1, 2, 3", "uno, dos, tres" ],
+				[ "4, 5, 6", "cuatro, cinco, seis" ],
+				[ "7, 8, 9", "siete, ocho, nueve" ]
+			]
+		}, {
+			name: "Más Números",
+			cards: [
+				[ "10, 11, 12", "diez, once, doce" ],
+				[ "13, 14, 15", "trece, catorce, quince" ],
+				[ "16, 17, 18", "dieciséis, diecisiete, dieciocho" ]
+			]
+		}, {
+			name: "Números grandes",
+			cards: [
+				[ "19, 20, 21", "diecinueve, veinte, veintiuno" ],
+				[ "30, 40, 50", "treinta, cuarenta, cincuenta" ],
+				[ "60, 70, 80", "sesenta, setenta, ochenta" ]
+			]
+		}, {
+			name: "Los Colores",
+			cards: [
+				[ "Red, Green, Blue", "Rojo, Verde, Azul" ],
+				[ "Purple, Orange, Pink", "Morado, Naranja, Rosado" ],
+				[ "Yellow, Black, White", "Amarillo, Negro, Blanco" ]
+			]
+		}, {
+			name: "Animals",
+			cards: [
+				[ "Cat, Dog, Mouse", "Gato, Perro, Ratón" ],
+				[ "Lion, Tiger, Bear", "León, Tigre, Oso" ],
+				[ "Fish, Bird, Snake", "Pez, Pájaro, Serpiente" ]
+			]
+		}, {
+			name: "Food",
+			cards: [
+				[ "Apple, Banana, Orange", "Manzana, Banana, Naranja" ],
+				[ "Pizza, Hamburger, Fries", "Pizza, Hamburguesa, Papas Fritas" ],
+				[ "Cake, Ice Cream, Pie", "Pastel, Helado, Tarta" ]
+			]
+		} ]
+	} ]
+}
